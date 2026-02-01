699d72e7e6a0f20b98945573147b5e1360c04da9
dexterhere-2k <deepakmirchandani.ai28@jecrc.ac.in>
2026-02-01 16:47:47 +0530

    spl init done

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,18 +1,20 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
+import { Keypair, Connection, Commitment, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
-
+import wallet from "../../wba-wallet.json"
+// import  {RpcAirdropOptions}  from "@metaplex-foundation/umi";
+// import ai
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
-
 (async () => {
     try {
         // Start here
-        // const mint = ???
+         const signature=await connection.requestAirdrop(keypair.publicKey,1*LAMPORTS_PER_SOL)
+        const mint = await createMint(connection,keypair,keypair.publicKey,keypair.publicKey,6)
+        console.log("heres your key" ,mint)
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
